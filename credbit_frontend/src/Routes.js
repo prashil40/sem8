84304ae53155980293f8568bc91cd35aa7c68b55
Prashil Tandel <prashil@auberginesolutions.com>
2021-03-07 17:44:15 +0530

    ➕ Add Pricing Page - Added Pricing page and sub component PricingCard - Displayed Pricing card dynamically from database

--- credbit_frontend/src/Routes.js
+++ credbit_frontend/src/Routes.js
@@ -7,6 +7,7 @@ import ForgotPassword from './components/user/ForgotPassword/ForgotPassword';
 import ResetPassword from './components/user/ResetPassword/ResetPassword';
 import AboutUs from './components/core/AboutUs/AboutUs';
 import Letters from './components/core/Letters/Letters';
+import Pricing from './components/core/Pricing/Pricing';
 
 const Routes = () => {
   return (
@@ -15,6 +16,7 @@ const Routes = () => {
         <Route path="/" exact component={Home}></Route>
         <Route path="/about" exact component={AboutUs}></Route>
         <Route path="/letters" exact component={Letters}></Route>
+        <Route path="/pricing" exact component={Pricing}></Route>
         <Route path="/signin" exact component={Signin}></Route>
         <Route path="/signup" exact component={Signup}></Route>
         <Route path="/forgot" exact component={ForgotPassword}></Route>
