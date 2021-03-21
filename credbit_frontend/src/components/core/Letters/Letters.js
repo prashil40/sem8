793052baebb300b219b5fc6207896d2ef184d20f
Jaimish Trivedi <jaimishtrivedi00@gmail.com>
2021-03-21 12:56:55 +0530

    💚 Show error image on Pricing and Letter page
     - Added new illustration in images folder
     - Conditional rendering on piricng and letter page based on error status
     - New styles for the error section

--- credbit_frontend/src/components/core/Letters/Letters.js
+++ credbit_frontend/src/components/core/Letters/Letters.js
@@ -6,176 +6,179 @@ import { Link } from 'react-router-dom';
 import LetterCard from '../../partials/LetterCard/LetterCard';
 import { getLetters } from '../helpers/LetterApiCall';
 import FileUpload from '../../partials/FileUpload/FileUpload';
+import illustration from '../../images/undraw_server_down_s4lk.svg';
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const options = {
-  position: 'bottom-right',
-  autoClose: 5000,
-  hideProgressBar: false,
-  closeOnClick: true,
-  pauseOnHover: true,
-  draggable: true,
-  progress: undefined,
+	position: 'bottom-right',
+	autoClose: 5000,
+	hideProgressBar: false,
+	closeOnClick: true,
+	pauseOnHover: true,
+	draggable: true,
+	progress: undefined,
 };
 
 const Letters = () => {
-  const [letters, setLetters] = useState([]);
-  const [error, setError] = useState(false);
-  const [selectedLetters, setSelectedLetters] = useState([]);
-  const [nextValue, setNext] = useState(false);
+	const [letters, setLetters] = useState([]);
+	const [error, setError] = useState(false);
+	const [selectedLetters, setSelectedLetters] = useState([]);
+	const [nextValue, setNext] = useState(false);
 
-  const [document, setDocument] = useState({ documentsArr: [] });
+	const [document, setDocument] = useState({ documentsArr: [] });
 
-  const updateDocument = (files) => {
-    setDocument({ ...document, documentsArr: files });
-  };
+	const updateDocument = (files) => {
+		setDocument({ ...document, documentsArr: files });
+	};
 
-  let letterCards = letters.map((letter, index) => {
-    letter.index = index;
-    return (
-      <LetterCard
-        key={index}
-        letter={letter}
-        letterIndex={index}
-        setSelectedLetters={setSelectedLetters}
-        selectedLetters={selectedLetters}
-      />
-    );
-  });
+	let letterCards = letters.map((letter, index) => {
+		letter.index = index;
+		return (
+			<LetterCard
+				key={index}
+				letter={letter}
+				letterIndex={index}
+				setSelectedLetters={setSelectedLetters}
+				selectedLetters={selectedLetters}
+			/>
+		);
+	});
 
-  const loadAllLetters = () => {
-    getLetters()
-      .then((data) => {
-        if (data.error) {
-          setError(data.error);
-          console.log(error);
-        } else {
-          setLetters(data);
-        }
-      })
-      .catch((err) => console.log(err));
-  };
+	const loadAllLetters = () => {
+		getLetters()
+			.then((data) => {
+				if (data.error) {
+					setError(data.error);
+					console.log(error);
+				} else {
+					setLetters(data);
+				}
+			})
+			.catch((err) => {
+				setError(true);
+				console.log(err);
+			});
+	};
 
-  useEffect(() => {
-    loadAllLetters();
-    console.log('Selected Letters', selectedLetters);
-  }, []);
+	useEffect(() => {
+		loadAllLetters();
+		console.log('Selected Letters', selectedLetters);
+	}, []);
 
-  const handleSubmit = () => {
-    console.log('Selected Letters', selectedLetters);
+	const handleSubmit = () => {
+		console.log('Selected Letters', selectedLetters);
 
-    if (selectedLetters.length === 0) {
-      toast.error('Select atleast one letter', options);
-    } else {
-      setNext(true);
-    }
-  };
+		if (selectedLetters.length === 0) {
+			toast.error('Select atleast one letter', options);
+		} else {
+			setNext(true);
+		}
+	};
 
-  const dynamicComponent = () => {
-    if (!nextValue) {
-      return (
-        <div
-          className={`${classes.our_services_area} ${classes.grey_bg_2} pt-120 pb-130`}
-        >
-          <div className={classes.container} id="letters">
-            <div className="row">{letterCards}</div>
-            <div className={`row ${classes.row_style}`}>
-              <div className={classes.about_button} onClick={handleSubmit}>
-                <Link
-                  className={classes.blue_btn}
-                  // to="login/"
-                >
-                  {' '}
-                  <span className="btn-text">
-                    Next <i className="far fa-long-arrow-right"></i>
-                  </span>{' '}
-                </Link>
-              </div>
-              <div
-                id="hideDiv"
-                className={classes.hide_div}
-                //   style="display:none;margin-top:18px;"
-              >
-                <img
-                  src="loader1.gif"
-                  id="loader"
-                  // style="height:20px;"
-                />
-                <label
-                  id="lblMsgVerify"
-                  // style="font-size:17px;"
-                ></label>
-              </div>
-            </div>
-          </div>
-          <div
-            className={`${classes.container} ${classes.selected_letters}`}
-            id="selectedLetters"
-            //   style="display:none;"
-          >
-            <div
-              className="col-lg-12"
-              // style="text-align:center;"
-              id="loaderV"
-            >
-              <img
-                src="Loader.gif"
-                //   style="width:20%;"
-              />
-              <h6>Please wait!</h6>
-            </div>
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <FileUpload
-          accept=".jpg,.jpeg,.png,.pdf"
-          label="Document"
-          updateFilesCb={updateDocument}
-        />
-      );
-    }
-  };
-  return (
-    <div>
-      <Header />
-      <main>
-        {/* <!-- breadcrumb-area-start --> */}
-        <div
-          className={classes.breadcrumb_area}
-          //   style="background-image:url('page/big_img/bg-14502568.jpeg');"
-        >
-          <div className={classes.container}>
-            <div className="row">
-              <div className={classes.col_xl_12}>
-                <div
-                  className={`${classes.breadcrumb_text} ${classes.text_center}`}
-                >
-                  <h1>Letters</h1>
-                  <ul className={classes.breadcrumb_menu}>
-                    <li>
-                      <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                      <span>Letters</span>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* <!-- breadcrumb-area-start -->*/}
+	const dynamicComponent = () => {
+		if (!nextValue) {
+			return (
+				<div className={`${classes.our_services_area} ${classes.grey_bg_2} pt-120 pb-130`}>
+					<div className={classes.container} id='letters'>
+						<div className='row'>{letterCards}</div>
+						<div className={`row ${classes.row_style}`}>
+							<div className={classes.about_button} onClick={handleSubmit}>
+								<Link
+									className={classes.blue_btn}
+									// to="login/"
+								>
+									{' '}
+									<span className='btn-text'>
+										Next <i className='far fa-long-arrow-right'></i>
+									</span>{' '}
+								</Link>
+							</div>
+							<div
+								id='hideDiv'
+								className={classes.hide_div}
+								//   style="display:none;margin-top:18px;"
+							>
+								<img
+									src='loader1.gif'
+									id='loader'
+									// style="height:20px;"
+								/>
+								<label
+									id='lblMsgVerify'
+									// style="font-size:17px;"
+								></label>
+							</div>
+						</div>
+					</div>
+					<div
+						className={`${classes.container} ${classes.selected_letters}`}
+						id='selectedLetters'
+						//   style="display:none;"
+					>
+						<div
+							className='col-lg-12'
+							// style="text-align:center;"
+							id='loaderV'>
+							<img
+								src='Loader.gif'
+								//   style="width:20%;"
+							/>
+							<h6>Please wait!</h6>
+						</div>
+					</div>
+				</div>
+			);
+		} else {
+			return (
+				<FileUpload accept='.jpg,.jpeg,.png,.pdf' label='Document' updateFilesCb={updateDocument} />
+			);
+		}
+	};
+	return (
+		<div>
+			<Header />
+			<main>
+				{/* <!-- breadcrumb-area-start --> */}
+				{!error && (
+					<>
+						<div
+							className={classes.breadcrumb_area}
+							//   style="background-image:url('page/big_img/bg-14502568.jpeg');"
+						>
+							<div className={classes.container}>
+								<div className='row'>
+									<div className={classes.col_xl_12}>
+										<div className={`${classes.breadcrumb_text} ${classes.text_center}`}>
+											<h1>Letters</h1>
+											<ul className={classes.breadcrumb_menu}>
+												<li>
+													<Link to='/'>Home</Link>
+												</li>
+												<li>
+													<span>Letters</span>
+												</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
 
-        {dynamicComponent()}
-        {/* <ToastContainer /> */}
-      </main>
-      <Footer />
-    </div>
-  );
+						{dynamicComponent()}
+					</>
+        )}
+        {error && (
+					<div className={classes.error_area}>
+						<img src={illustration} alt='Error Illustration' />
+						<span>Server Down. Try again later!</span>
+					</div>
+				)}
+			</main>
+			<Footer />
+		</div>
+	);
 };
 
 export default Letters;
